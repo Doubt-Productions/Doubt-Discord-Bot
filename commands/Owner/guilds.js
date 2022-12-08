7b3b51570693f6cdc55fa170b634368affb7a809
zVapor_ <zvapor231@gmail.com>
2022-12-09 00:07:37 +0100

    added guilds.js

---
+++ commands/Owner/guilds.js
@@ -0,0 +1,29 @@
+const {
+  EmbedBuilder,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  ChatInputCommandInteraction,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("guilds")
+    .setDescription("Get a list of all guilds the bot is in.")
+    .setDefaultMemberPermissions(0),
+  category: "General",
+  /**
+   *
+   * @param {*} client
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {*} config
+   */
+  run: async (client, interaction, config) => {
+    const guilds = client.guilds.cache.map((guild) => guild.name);
+    const embed = new EmbedBuilder()
+      .setTitle("Guilds")
+      .setDescription(guilds.join("\n"))
+      .setColor("Random")
+      .setTimestamp();
+    interaction.reply({ embeds: [embed] });
+  },
+};
