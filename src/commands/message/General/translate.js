c0ad003f4b32c53f9d5b31e8ef569579a5320ae6
zVapor_ <zvapor231@gmail.com>
2023-09-16 23:31:50 +0200

    added translate command

---
+++ src/commands/message/General/translate.js
@@ -0,0 +1,38 @@
+const {
+  MessageContextMenuCommandInteraction,
+  ContextMenuCommandBuilder,
+  EmbedBuilder,
+} = require("discord.js");
+const ExtendedClient = require("../../../class/ExtendedClient");
+const translate = require("@iamtraction/google-translate");
+
+module.exports = {
+  structure: new ContextMenuCommandBuilder()
+    .setName("Translate Message")
+    .setType(3),
+  /**
+   * @param {ExtendedClient} client
+   * @param {MessageContextMenuCommandInteraction} interaction
+   */
+  run: async (client, interaction) => {
+    const message = interaction.targetMessage;
+
+    if (!message) {
+      return;
+    }
+
+    const translatedMessage = translate(message.content, { to: "en" });
+
+    const embed = new EmbedBuilder()
+      .setColor("Blurple")
+      .setAuthor(
+        message.author.displayName,
+        message.author.displayAvatarURL({ dynamic: true })
+      )
+      .setTitle("🔍 Translation Successful!")
+      .addFields({ name: "Original Message", value: message.content })
+      .addFields({ name: "Translated Message", value: translatedMessage });
+
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+  },
+};
