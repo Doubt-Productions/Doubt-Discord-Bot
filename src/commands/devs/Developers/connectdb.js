9c010327173f35adb839e514ca26d602ea6a4fdd
zVapor_ <zvapor231@gmail.com>
2023-09-17 00:00:17 +0200

    added dev command

---
+++ src/commands/devs/Developers/connectdb.js
@@ -0,0 +1,39 @@
+const {
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  EmbedBuilder,
+} = require("discord.js");
+const ExtendedClient = require("../../../class/ExtendedClient");
+const config = require("../../../config");
+const GuildSchema = require("../../../schemas/GuildSchema");
+const { log } = require("../../../functions");
+const mongoose = require("../../../handlers/mongoose");
+
+module.exports = {
+  structure: new SlashCommandBuilder()
+    .setName("conncetdb")
+    .setDescription("Attempt to connect the DB."),
+  options: {
+    developers: true,
+  },
+  /**
+   * @param {ExtendedClient} client
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {[]} args
+   */
+  run: async (client, interaction, args) => {
+    try {
+      await interaction.deferReply();
+      await mongoose().catch((err) => {
+        log(err, "err");
+        interaction.editReply(
+          "An error occurred while connecting to the database."
+        );
+      });
+      interaction.editReply("Successfully connected to the database.");
+    } catch (error) {
+      log(error, "err");
+      interaction.reply("An error occurred while connecting to the database.");
+    }
+  },
+};
