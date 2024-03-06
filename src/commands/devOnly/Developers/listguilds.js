5c0a899ad33bdd4bf8e1760c36731e020b59c55d
zVapor-Dev <zvapor231@gmail.com>
2024-03-06 03:41:18 +0100

    bug fixes

--- src/commands/devOnly/Developers/listguilds.js
+++ src/commands/devOnly/Developers/listguilds.js
@@ -19,42 +19,35 @@ module.exports = {
    * @param {[]} args
    */
   run: async (client, interaction, args) => {
-    const guildArray = [];
-
-    const guilds = client.guilds.cache;
-    const guildChunks = guilds.reduce(
-      (acc, guild) => {
-        const chunk = acc[acc.length - 1];
-        if (chunk.length < 10) {
-          chunk.push(guild);
-        } else {
-          acc.push([guild]);
-        }
-        return acc;
-      },
-      [[]]
-    );
-
-    const page = interaction.options.getInteger("page") || 1;
-    const guildChunk = guildChunks[page - 1];
-
-    const embed = new EmbedBuilder()
-      .setColor("#0099ff")
-      .setTitle("Guild List")
-      .setDescription(`Page ${page}/${guildChunks.length}`);
-
-    guildChunk.forEach((guild) => {
-      embed.addFields({ name: `${guild.name}`, value: `ID: ${guild.id}` });
+    const guilds = client.guilds.cache.map((guild) => {
+      return {
+        name: guild.name,
+        id: guild.id,
+        memberCount: guild.memberCount,
+        owner: guild.ownerId,
+      };
     });
 
-    if (guildChunks.length > 1) {
-      guildArray.push(embed);
+    const guildsArray = [];
+    for (let i = 0; i < guilds.length; i += 10) {
+      const current = guilds.slice(i, i + 10);
+      guildsArray.push(current);
     }
 
-    if (guildArray.length > 1) {
-      await buttonPagination(interaction, guildArray);
-    }
+    const pages = guildsArray.map((guilds, index) => {
+      const description = guilds
+        .map(
+          (guild) =>
+            `**${guild.name}**\nID: ${guild.id}\nMember Count: ${guild.memberCount}\nOwner: <@${guild.owner}>`
+        )
+        .join("\n\n");
 
-    interaction.reply({ embeds: [embed] });
+      return new EmbedBuilder()
+        .setTitle(`Guilds List - Page ${index + 1}/${guildsArray.length}`)
+        .setDescription(description)
+        .setColor("Green");
+    });
+
+    await buttonPagination(interaction, pages, 30000);
   },
 };
