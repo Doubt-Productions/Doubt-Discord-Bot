9575e61bb00906d10a4192875cd314fcef9286be
zVapor-Dev <zvapor231@gmail.com>
2024-03-06 03:02:26 +0100

    some basic fixes

--- src/example.config.js
+++ src/example.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   client: {
     token: process.env.PRODUCTION === true
-      ? process.env.DEV_TOKEN
-      : process.env.CLIENT_TOKEN,
+      ? process.env.CLIENT_TOKEN
+      : process.env.DEV_TOKEN,
     id: process.env.PRODUCTION === true
-      ? process.env.DEV_CLIENT_ID
-      : process.env.CLIENT_ID,
+      ? process.env.CLIENT_ID
+      : process.env.DEV_CLIENT_ID,
   },
   variables: {
     channels: {
@@ -17,7 +17,7 @@ module.exports = {
         ? ""
         : "",
     },
-    dbName: process.env.PRODUCTION === true ? "development" : "production",
+    dbName: process.env.PRODUCTION ? "production" : "development",
     supportServerId: process.env.GUILD_ID,
   },
   moderation: {
@@ -27,10 +27,10 @@ module.exports = {
   handler: {
     prefix: "?",
     deploy: true,
-    guildDeploy: process.env.PRODUCTION === true ? true : false,
+    guildDeploy: true,
     guildId: process.env.PRODUCTION === true
-      ? process.env.DEV_GUILD_ID
-      : process.env.GUILD_ID,
+      ? process.env.GUILD_ID
+      : process.env.DEV_GUILD_ID,
     commands: {
       prefix: false,
       slash: true,
@@ -38,9 +38,9 @@ module.exports = {
       message: true,
     },
     mongodb: {
-      uri: process.env.PRODUCTION === true
-        ? process.env.DEV_MONGODB_URI
-        : process.env.MONGODB_URI,
+      uri: process.env.PRODUCTION
+        ? process.env.MONGODB_URI
+        : process.env.DEV_MONGODB_URI,
       toggle: true,
     },
   },
