4a36946a4521294adb62ef4e6f7256d57dc26366
zVapor-Dev <zvapor231@gmail.com>
2024-03-15 04:31:59 +0100

    added channel select menu for tickets

--- src/components/selects/ticketSSM.js
+++ src/components/selects/ticketSSM.js
@@ -93,6 +93,62 @@ module.exports = {
         });
 
         break;
+
+      case "channel":
+        const ticketCSM2 = new ChannelSelectMenuBuilder()
+          .setCustomId("ticketCSM2")
+          .setPlaceholder("Select a channel")
+          .setChannelTypes(ChannelType.GuildText)
+          .setMinValues(1)
+          .setMaxValues(1);
+
+        const ticketRow2 = new ActionRowBuilder().addComponents(ticketCSM2);
+
+        embed.setDescription(
+          "Please select a channel where you want to create tickets!"
+        );
+
+        const reply2 = await interaction.update({
+          embeds: [embed],
+          components: [ticketRow2, goBackRow],
+          fetchReply: true,
+        });
+
+        const filter2 = (i) => i.customId === "ticketCSM2";
+        const collector2 = reply2.createMessageComponentCollector({
+          filter: filter2,
+          time: 60000,
+        });
+
+        collector2.on("collect", async (i) => {
+          const channel = i.values[0];
+          const guild = i.guild;
+          const channel2 = guild.channels.cache.get(channel);
+
+          if (!channel2) {
+            return i.reply({
+              content: "Please select a valid channel!",
+              ephemeral: true,
+            });
+          }
+
+          if (ticketData) {
+            ticketData.Channel = channel;
+            ticketData.save();
+          } else {
+            new ticketSchema({
+              Guild: guild.id,
+              Channel: channel,
+            }).save();
+          }
+
+          await i.reply({
+            content: "Channel has been set!",
+            ephemeral: true,
+          });
+        });
+
+        break;
     }
   },
 };
