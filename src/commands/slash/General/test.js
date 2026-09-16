6632bc32efa33b38b323190a002d0814e830b3fe
zVapor_ <zvapor231@gmail.com>
2026-09-17 00:40:00 +0200

    Enhance /test command to provide a personalized welcome message with join timestamp. Refactor command structure to include necessary imports and error handling for interaction replies.

--- src/commands/slash/General/test.js
+++ src/commands/slash/General/test.js
@@ -1,12 +1,39 @@
-
-const { SlashCommandBuilder } = require('discord.js');
+const {
+  SlashCommandBuilder,
+  ChatInputCommandInteraction,
+} = require("discord.js");
+const { Profile } = require("discord-arts");
+const ExtendedClient = require("../../../class/ExtendedClient");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('test')
-    .setDescription('A test command.')
+    .setName("test")
+    .setDescription("A test command.")
     .toJSON(),
+  /**
+   *
+   * @param {ExtendedClient} client
+   * @param {ChatInputCommandInteraction} interaction
+   * @returns
+   */
   run: async (client, interaction) => {
-    await interaction.reply('This is a test command!');
+    // await interaction.reply("This is a test command!");
+    const member = interaction.guild.members.cache.get(interaction.user.id);
+    const guild = interaction.guild;
+
+    console.log();
+
+    try {
+      const timestamp = `<t:${
+        Math.round(member.joinedTimestamp / 1000)
+      }:D>`;
+
+      return interaction.reply({
+        content: `Welcome to the server! You joined on ${timestamp}`,
+      });
+    } catch (error) {
+      console.error("Error generating welcome image:", error);
+      throw error;
+    }
   },
 };
